fa4926b08498ce2b9940107e38ef3bd2c25ac3a3
AlexKovalchuk <kovalchuk111@gmail.com>
2023-06-27 05:47:30 +0300

    Hw #24

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,26 @@
+package telran.text;
+
+import java.lang.module.ModuleDescriptor.Builder;
+
+public class Strings {
+
+	public static String javaVariableName() {
+		return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	}
+	
+	public static String zero_300() {
+		return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+	}
+
+	public static String ipV4Octet() {
+		return "\\d\\d?|[0-1]\\d\\d|[0-2][0-4]\\d|[0-2][0-5][0-5]";
+	}
+	
+	public  static String ipV4() {
+//		return "(([0-9][0-9]?|[0-1]\\d\\d|[0-2][0-4]\\d|[0-2][0-5][0-5]).)"
+//				+ "{3}" + "(" + "[0-9][0-9]?|[0-1]\\d\\d|[0-2][0-4]\\d|[0-2][0-5][0-5]" + ")";
+		String regOctet = ipV4Octet(); 
+		return "((" + regOctet + ").){3}(" + regOctet + ")";
+	}
+}
+  
